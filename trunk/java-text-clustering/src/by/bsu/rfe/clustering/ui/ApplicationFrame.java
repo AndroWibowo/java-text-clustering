3b35b3220c340f1ab6487cf2e3c68f47c960a8fe
quasisergei@gmail.com <quasisergei@gmail.com@a5dec423-57dd-6eb8-cdaa-e3e78254f43a>
2011-03-29 17:27:01 +0000

    Start UI implementation

---
+++ trunk/java-text-clustering/src/by/bsu/rfe/clustering/ui/ApplicationFrame.java
@@ -0,0 +1,38 @@
+package by.bsu.rfe.clustering.ui;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JSplitPane;
+
+public class ApplicationFrame extends JFrame {
+
+    private static final long serialVersionUID = -3331823388635050038L;
+
+    private static final int DEFAULT_WIDTH = 640;
+    private static final int DEFAULT_HEIGHT = 480;
+
+    private final JSplitPane _mainPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+
+    public ApplicationFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        center();
+        init();
+    }
+
+    private void init() {
+        getContentPane().add(_mainPanel);
+    }
+
+    private void center() {
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+
+        setLocation(screenWidth / 2 - getWidth() / 2, screenHeight / 2 - getHeight() / 2);
+    }
+
+}
