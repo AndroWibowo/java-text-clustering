425b701150ce871df73080bc1a34888878cd41a2
quasisergei@gmail.com <quasisergei@gmail.com@a5dec423-57dd-6eb8-cdaa-e3e78254f43a>
2011-04-01 18:31:50 +0000

    common class WeightedValue

---
+++ trunk/java-text-clustering/src/by/bsu/rfe/clustering/math/WeightedValue.java
@@ -0,0 +1,28 @@
+package by.bsu.rfe.clustering.math;
+
+public class WeightedValue<V> implements Comparable<WeightedValue<V>> {
+    private V _value;
+    private double _weight;
+
+    public WeightedValue(V value, double weight) {
+        _value = value;
+        _weight = weight;
+    }
+
+    public static <V> WeightedValue<V> of(V value, double weight) {
+        return new WeightedValue<V>(value, weight);
+    }
+
+    public double weight() {
+        return _weight;
+    }
+
+    public V value() {
+        return _value;
+    }
+
+    @Override
+    public int compareTo(WeightedValue<V> anotherValue) {
+        return Double.compare(weight(), anotherValue.weight());
+    }
+}
