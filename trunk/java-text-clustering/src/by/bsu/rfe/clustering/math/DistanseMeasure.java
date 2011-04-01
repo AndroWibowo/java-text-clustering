e247fb4c86a2a7b40391cecd85df773f22174250
quasisergei@gmail.com <quasisergei@gmail.com@a5dec423-57dd-6eb8-cdaa-e3e78254f43a>
2011-04-01 23:55:34 +0000

    Refactoring

--- trunk/java-text-clustering/src/by/bsu/rfe/clustering/math/DistanseMeasure.java
+++ trunk/java-text-clustering/src/by/bsu/rfe/clustering/math/DistanseMeasure.java
@@ -1,6 +1,6 @@
 package by.bsu.rfe.clustering.math;
 
-public interface VectorDistanseMeasure {
+public interface DistanseMeasure {
 
     public double compute(DoubleVector vector1, DoubleVector vector2);
 
