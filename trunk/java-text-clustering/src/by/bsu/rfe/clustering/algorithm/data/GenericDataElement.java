2a7566f3f13fc3d6364b8e3c0bf2f1a07891b6db
quasisergei@gmail.com <quasisergei@gmail.com@a5dec423-57dd-6eb8-cdaa-e3e78254f43a>
2011-04-03 22:54:05 +0000

    Extract interface DataElement

---
+++ trunk/java-text-clustering/src/by/bsu/rfe/clustering/algorithm/data/GenericDataElement.java
@@ -0,0 +1,19 @@
+package by.bsu.rfe.clustering.algorithm.data;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import by.bsu.rfe.clustering.math.DoubleVector;
+
+public class GenericDataElement implements DataElement {
+
+    private DoubleVector _vector;
+
+    public GenericDataElement(DoubleVector vector) {
+        _vector = checkNotNull(vector, "Vector is null");
+    }
+
+    @Override
+    public DoubleVector asVector() {
+        return _vector;
+    }
+
+}
