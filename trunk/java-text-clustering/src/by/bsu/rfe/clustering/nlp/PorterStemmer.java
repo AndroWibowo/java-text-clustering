03877d5be7ded8c1cc884ac3ca15371fe7d915ab
quasisergei@gmail.com <quasisergei@gmail.com@a5dec423-57dd-6eb8-cdaa-e3e78254f43a>
2011-04-11 17:56:52 +0000

    add Porter stemmer implementation

---
+++ trunk/java-text-clustering/src/by/bsu/rfe/clustering/nlp/PorterStemmer.java
@@ -0,0 +1,17 @@
+package by.bsu.rfe.clustering.nlp;
+
+public class PorterStemmer implements Stemmer {
+
+    private PorterStemmerImpl _porterStemmer;
+
+    public PorterStemmer() {
+        _porterStemmer = new PorterStemmerImpl();
+    }
+
+    @Override
+    public String stem(String token) {
+        _porterStemmer.add(token.toCharArray(), token.length());
+        _porterStemmer.stem();
+        return _porterStemmer.toString();
+    }
+}
