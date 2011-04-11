0b67e8b58c4a8cfdc21ea9c748f7b9dbe22c3891
quasisergei@gmail.com <quasisergei@gmail.com@a5dec423-57dd-6eb8-cdaa-e3e78254f43a>
2011-04-11 22:23:58 +0000

    fix compilation errors in "test classes"

--- java-text-clustering/src/test/by/bsu/rfe/clustering/app/cli/CLI.java
+++ java-text-clustering/src/test/by/bsu/rfe/clustering/app/cli/CLI.java
@@ -8,12 +8,12 @@ import org.apache.commons.logging.LogFactory;
 
 import test.by.bsu.rfe.clustering.app.util.CSVDataSetExporter;
 
-import by.bsu.rfe.clustering.algorithm.ClusteringAlgorithm;
+import by.bsu.rfe.clustering.algorithm.Clustering;
 import by.bsu.rfe.clustering.algorithm.cluster.Cluster;
 import by.bsu.rfe.clustering.math.DistanseMeasure;
 import by.bsu.rfe.clustering.math.EuclideanDistanceMeasure;
 import by.bsu.rfe.clustering.nlp.WordList;
-import by.bsu.rfe.clustering.text.algorithm.TextKMeansAlgorithm;
+import by.bsu.rfe.clustering.text.algorithm.TextKMeansClustering;
 import by.bsu.rfe.clustering.text.document.DocumentCollection;
 import by.bsu.rfe.clustering.text.document.DocumentDataElement;
 import by.bsu.rfe.clustering.text.document.DocumentDataSet;
@@ -31,7 +31,7 @@ public class CLI {
         File stopWords = new File("dictionary\\stopwords.txt");
         WordList stopWordList = WordList.load(stopWords);
 
-        DocumentCollectionReader reader = new FileSystemDocumentCollectionReader(new File("samples"), stopWordList);
+        DocumentCollectionReader reader = new FileSystemDocumentCollectionReader(new File("h:\\samples"), stopWordList);
 
         DocumentCollection docCollection = reader.readDocuments();
 
@@ -47,17 +47,15 @@ public class CLI {
 
         CSVDataSetExporter.export(dataSet, new File("tmp\\dataset.csv"));
 
-        DistanseMeasure distanse = new EuclideanDistanceMeasure();
+        final int numberOfClusters = 2;
 
-        final int numberOfClusters = 100;
-
-        ClusteringAlgorithm<DocumentDataElement, Cluster<DocumentDataElement>, DocumentDataSet> clustering = new TextKMeansAlgorithm(
-                distanse, numberOfClusters);
+        Clustering<DocumentDataElement, Cluster<DocumentDataElement>, DocumentDataSet> clustering = new TextKMeansClustering(
+                numberOfClusters);
 
         List<Cluster<DocumentDataElement>> clusters = clustering.cluster(dataSet);
 
         for (Cluster<DocumentDataElement> cluster : clusters) {
-            System.out.printf("%n(%d) %s:%n%n",cluster.getDataElements().size(), cluster.getLabel());
+            System.out.printf("%n(%d) %s:%n%n", cluster.getDataElements().size(), cluster.getLabel());
 
             for (DocumentDataElement elem : cluster.getDataElements()) {
                 System.out.printf("\t%s%n", elem.getDocument().getTitle());
