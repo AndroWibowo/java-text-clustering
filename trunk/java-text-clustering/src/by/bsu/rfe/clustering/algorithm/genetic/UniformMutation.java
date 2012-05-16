2061a08457b05f0f47df54a2cef5a1176f95e271
quasisergei@gmail.com <quasisergei@gmail.com@a5dec423-57dd-6eb8-cdaa-e3e78254f43a>
2012-05-16 15:10:25 +0000

    Some commit

--- trunk/java-text-clustering/src/by/bsu/rfe/clustering/algorithm/genetic/UniformMutation.java
+++ trunk/java-text-clustering/src/by/bsu/rfe/clustering/algorithm/genetic/UniformMutation.java
@@ -9,27 +9,24 @@ public final class UniformMutation implements MutationStrategy {
   private int lowerBound;
   private int uppperBound;
   private double rate;
-  private double geneRate;
 
-  public UniformMutation(int loweBound, int upperBound, double rate, double geneRate) {
+  public UniformMutation(int loweBound, int upperBound, double rate) {
     this.lowerBound = loweBound;
     this.uppperBound = upperBound;
     this.rate = rate;
-    this.geneRate = geneRate;
   }
 
   @Override
   public void mutate(List<Chromosome> population) {
     for (Chromosome c : population) {
-      final double r = random.nextDouble();
 
-      if (r <= rate) {
-        final int[] genes = c.getGenes();
+      final int[] genes = c.getGenes();
 
-        for (int i = 0; i < genes.length; i++) {
-          if (random.nextDouble() <= geneRate) {
-            genes[i] = lowerBound + random.nextInt(uppperBound - lowerBound);
-          }
+      for (int i = 0; i < genes.length; i++) {
+        if (random.nextDouble() <= rate) {
+          genes[i] = lowerBound + random.nextInt(uppperBound - lowerBound);
+
+          System.out.printf("\tGene[%d] Mutated to %d%n", i, genes[i]);
         }
       }
     }
